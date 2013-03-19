e15c8ca8b91c498577308dfe9c674faca91508dd
Apostolis <ap.giannakidis@gmail.com>
2013-03-19 03:09:37 +0000

    added cloud simulation and send it to Drools server

--- client/src/main/java/client/Client.java
+++ client/src/main/java/client/Client.java
@@ -2,17 +2,23 @@ package client;
 
 import java.io.*;
 import java.net.*;
+
+import model.Cloud;
+
 import org.apache.log4j.*;
+
+import com.google.gson.Gson;
 
 public class Client {
 	
-	private int PORT = 9070;
+	private int PORT = 2111;
 	private String HOST = "localhost";
     private Socket socket = null;
     private BufferedReader in;
     private BufferedWriter out;
     private static Client _instance = null;
 	private static final Logger LOG = Logger.getLogger(Client.class.getCanonicalName());
+	private Gson gson = null;
 	
 	{
 		LOG.setLevel(Main.getLogLevel());
@@ -37,6 +43,7 @@ public class Client {
 		} catch (IOException e){
 			e.printStackTrace();
 		}
+		gson = new Gson();
     }
     
     public static Client getInstance(){
@@ -46,9 +53,11 @@ public class Client {
     	return _instance;
     }
     
-    public void sendToServer(String s){
+    public void sendToServer(Cloud cloud){
+    	String s = null;
     	try {
     		if (socket.isConnected()){
+    			s = gson.toJson(cloud);
     			out.write(s);
     			out.flush();
     		}
--- client/src/main/java/client/Main.java
+++ client/src/main/java/client/Main.java
@@ -1,8 +1,14 @@
 package client;
 
+import java.util.Map;
+import model.Blade;
+import model.Cloud;
+
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+
+import com.google.gson.Gson;
 
 /**
  * @author Apostolos Giannakidis
@@ -11,14 +17,26 @@ import org.apache.log4j.Logger;
 public class Main {
     
 	private static final Logger LOG = Logger.getLogger(Main.class.getCanonicalName());
-
+	private static Cloud cloud = null;
+	private static Client client = null;
+	
 	public static void main(String[] args) {
 			
 		setupLogger(args);
 		
-		Client client = Client.getInstance();
+		setupServer();
 		
-		client.sendToServer("test");
+		setupCloud();
+		
+		client.sendToServer(cloud);
+		/*
+		Gson gson = new Gson();
+		
+		String s = gson.toJson(cloud);
+		
+		LOG.info(s);
+		
+		Cloud cloud2 = gson.fromJson(s, Cloud.class)*/
 	}
 	
 	private static void setupLogger(String[] args){
@@ -30,7 +48,36 @@ public class Main {
 		LOG.info("Client initialising...");
 		LOG.info("Log level is set to: "+LOG.getLevel());
 	}
+	
+	private static void setupServer(){
+		client = Client.getInstance();
+	}
 
+	private static void setupCloud(){
+		cloud = new Cloud();
+		
+		/***** Simulation FIXME ******/
+		long id = 1;
+		double memory_total = 100;
+		double memoryUsage_current_MB = 50;
+		double disk_total_GB = 100;
+		double diskUsage_current_GB = 50;
+		double networkBandwidthUsed_KBs = 100;
+		boolean on = true;
+		double maximumNetworkBandwidth_KBs = 1000;
+		
+		Blade blade = new Blade(id, memory_total,
+				memoryUsage_current_MB,
+				disk_total_GB,
+				diskUsage_current_GB,
+				networkBandwidthUsed_KBs,
+				on,
+				maximumNetworkBandwidth_KBs);
+		
+		cloud.setBlade(id, blade);
+		/****************************/
+	}
+	
 	public static Level getLogLevel() {
 		return LOG.getLevel();
 	}
